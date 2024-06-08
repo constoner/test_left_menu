d2221778e1442e834912b8ae08444d4697a45120
constoner <constantin.kalinin@gmail.com>
2024-06-08 17:42:31 +0300

    create main menu with simple submenus

--- js/index.js
+++ js/index.js
@@ -1,7 +1,11 @@
 import { initUser } from "./user-menu.js";
 import { setCounters } from "./counters.js";
+import { initMenu } from "./main-menu.js";
 import { initErrorButton } from "./error-msg.js";
 
 initUser();
 setCounters();
+initMenu();
 initErrorButton();
+
+
